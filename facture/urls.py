cf319e50c913c47d8a60f5b1bc5830e6837e48b4
chanel <chanelbirimbi@gmail.com>
2025-05-03 21:39:54 +0100

    Utiliser i18n_patterns pour gérer les préfixes de langue

--- facture/urls.py
+++ facture/urls.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('', include('facture_app.urls')),
+    path('i18n/', include('django.conf.urls.i18n'))
 ]
 
-# Serve media files during development
+urlpatterns += i18n_patterns(
+    path('', include('facture_app.urls')),
+    prefix_default_language=False
+)
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
