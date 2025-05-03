4f688535f252f57f005c2314a0c41ab8b9f5da47
chanel <chanelbirimbi@gmail.com>
2025-05-03 21:22:02 +0100

    Ajouter URL set_language pour i18n et restaurer service des fichiers statiques

--- facture/urls.py
+++ facture/urls.py
@@ -2,22 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 
-# Base URL patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-# Internationalized URL patterns
-urlpatterns += i18n_patterns(
+    path('i18n/', include('django.conf.urls.i18n')),
     path('', include('facture_app.urls')),
-)
+]
 
 # Serve media files during development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Custom error handlers
 handler404 = 'facture_app.views.custom_404'
 handler500 = 'facture_app.views.custom_500'
+
